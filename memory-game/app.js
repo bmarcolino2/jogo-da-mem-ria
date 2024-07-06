16cfd57ea2522418718e0b73856c447124544c85
Bruna Rayane Marcolino da Silva <166544258+bmarcolino2@users.noreply.github.com>
2024-07-06 17:46:16 -0300

    att

---
+++ memory-game/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Board from './components/Board';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Jogo da Memória</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
